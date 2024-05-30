dae31961de01f8a7c40fc5c7d1b1c2ecb5d0dcb3
Om Preetham <ompreethambandi@gmail.com>
2024-05-30 14:08:16 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Om Preetham Bandi on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
